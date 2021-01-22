fffe3387024c0b1dc75b22037cadee50e36fef6f
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-22 09:34:18 -0600

    challenge 1 complete - where do I belong

--- script.js
+++ script.js
@@ -5,3 +5,18 @@
 // Challenge 1
 // Where do I belong? (Return the lowest index at which a value should be inserted into an array once it has been sorted)
 
+const getIndexToIns = (arr, num) => {
+    arr.sort((a, b) => {
+        return a - b
+    })
+
+    for (let i=0; i < arr.length; i++) {
+        if (num <= arr[i]) {
+            return i;
+        }
+    }
+    return arr.length
+}
+
+console.log(getIndexToIns([40, 60, 5], 50))
+
