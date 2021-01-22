3bf7cb5817b29b53a1dc57a9b64614c82fedecb2
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-22 09:58:43 -0600

    challenge 3 completed - diff two arrays

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ const sumAll = (arr) => {
 
 console.log(sumAll([1, 4]))
 // expected outcome 10
+
+// Challenge 3
+// Diff Two Arrays (compare 2 arrays and return a new array with any items only found in one of the 2 given arrays but not both)
+
+const diffArray = (arr1, arr2) => {
+    let newArr = []
+
+    for (let i=0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            newArr.push(arr1[i])
+        }
+    }
+    for (let j=0; j < arr2.length; j++) {
+        if (arr1.indexOf(arr2[j]) === -1) {
+            newArr.push(arr2[j])
+        }
+    }
+    return newArr
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
+// expected outcome [4]
