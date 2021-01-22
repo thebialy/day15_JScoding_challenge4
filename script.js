6110b86b9aed51bb5992a90a8d22d107520fe3e8
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-22 09:49:18 -0600

    challenge 2 completed - sum all numbers in a range

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ const getIndexToIns = (arr, num) => {
 
 console.log(getIndexToIns([40, 60, 5], 50))
 
+// Challenge 2
+// Sum All Numbers in a Range
+
+const sumAll = (arr) => {
+    let start = Math.min(arr[0], arr[1])
+    let finish = Math.max(arr[0], arr[1])
+    let total = 0
+
+    for (let i=start; i <= finish; i++) {
+        total += i
+    }
+    return total
+}
+
+console.log(sumAll([1, 4]))
