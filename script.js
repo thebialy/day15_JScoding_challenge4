ab663953cc0ba0bf652e9addf385ae144ce7a760
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-22 09:10:55 -0600

    freeCodeCamp JS challenges day 4 started

---
+++ script.js
@@ -0,0 +1,7 @@
+/////////////////////////////////
+// freeCodeCamp JS Challenges //
+///////////////////////////////
+
+// Challenge 1
+// Where do I belong? (Return the lowest index at which a value should be inserted into an array once it has been sorted)
+
